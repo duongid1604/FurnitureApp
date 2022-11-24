1549895ce8b7254d43964ff6c7208e4321c06ecf
Dylan Nguyen <duong16042000@gmail.com>
2022-11-24 14:21:59 +0700

    8-Add-Boarding-Screen.#8

---
+++ src/screens/BoardingScreen/index.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const BoardingScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>BoardingScreen</Text>
+    </View>
+  );
+};
+
+export default BoardingScreen;
+
+const styles = StyleSheet.create({});
