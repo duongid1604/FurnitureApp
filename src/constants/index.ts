93061295acb1cca6c19dddf022fade576bf240cf
TranVietNam <vietnamtudonghoa98@gmail.com>
2022-11-24 15:30:11 +0700

    finish add font to project.#17

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export {default as FONTS} from './Fonts';
+export {default as COLORS} from './Colors';
+export {default as FONT_SIZE} from './FontSize';
+export {default as FONT_WEIGHT} from './FontWeight';
