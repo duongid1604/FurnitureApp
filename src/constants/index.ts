e8aaf36d6f55f20501cca88ffb18603f102d6f37
Dylan Nguyen <duong16042000@gmail.com>
2022-11-24 16:58:06 +0700

    Add-Figma-Icon-#21

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export {default as FONTS} from './Fonts';
 export {default as COLORS} from './Colors';
 export {default as FONT_SIZE} from './FontSize';
 export {default as FONT_WEIGHT} from './FontWeight';
+export {default as ICON} from './Icon';
