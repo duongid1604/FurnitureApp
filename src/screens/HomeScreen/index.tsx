3aa2962b24d6f518d16ae5851757caf960f829ff
Dylan Nguyen <duong16042000@gmail.com>
2022-11-25 16:16:36 +0700

    Add-Home-Screen-#35(50%)

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 import {HomeScreenProps} from '../../types';
-import {TestButton} from '../../components';
+import {CustomScreenContainer, TestButton} from '../../components';
+import {COLORS, FONTS, FONT_SIZE, FONT_WEIGHT, ICON} from '../../constants';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = ({navigation}: HomeScreenProps) => {
   const moveToProductScreenHandler = () => {
@@ -9,13 +11,79 @@ const HomeScreen = ({navigation}: HomeScreenProps) => {
   };
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <TestButton name="Move To Product" onPress={moveToProductScreenHandler} />
-    </View>
+    <CustomScreenContainer>
+      <View style={styles.header}>
+        <Image source={ICON.SEARCH_DISABLE} style={styles.image} />
+        <View style={styles.headingCenter}>
+          <Text style={styles.smallHeading}>make home</Text>
+          <Text style={styles.bigHeading}>beautiful</Text>
+        </View>
+        <Image source={ICON.BI_CART_DISABLE} style={styles.image} />
+      </View>
+
+      <View style={styles.categoryContainer}>
+        <View style={styles.backgroundCategory}>
+          <Icon name="star-o" style={styles.category} />
+        </View>
+        <View style={styles.backgroundCategory}>
+          <Icon name="star-o" style={styles.category} />
+        </View>
+        <View style={styles.backgroundCategory}>
+          <Icon name="star-o" style={styles.category} />
+        </View>
+        <View style={styles.backgroundCategory}>
+          <Icon name="star-o" style={styles.category} />
+        </View>
+        <View style={styles.backgroundCategory}>
+          <Icon name="star-o" style={styles.category} />
+        </View>
+      </View>
+    </CustomScreenContainer>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  image: {
+    width: 24,
+    height: 24,
+  },
+  headingCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  smallHeading: {
+    textTransform: 'uppercase',
+    color: COLORS.SUB,
+    fontSize: FONT_SIZE.BODY,
+    fontFamily: FONTS.POPPINS,
+    fontWeight: FONT_WEIGHT.REGULAR,
+  },
+  bigHeading: {
+    textTransform: 'uppercase',
+    color: COLORS.SUB,
+    fontSize: FONT_SIZE.H4,
+    fontFamily: FONTS.POPPINS_BOLD,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  backgroundCategory: {
+    backgroundColor: COLORS.DISABLE,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+  },
+  category: {
+    fontSize: 32,
+  },
+});
