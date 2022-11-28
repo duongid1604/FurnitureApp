60d106d82d41bfc3edfc4e50e80ff3520758c14b
Dylan Nguyen <duong16042000@gmail.com>
2022-11-28 15:15:04 +0700

    Add Firebase Call api

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,2 +1,3 @@
 export {default as exampleReducer} from './exampleSlice';
 export {default as authReducer} from './authSlice';
+export {default as productsReducer} from './productSlice';
--- src/types/redux.types.ts
+++ src/types/redux.types.ts
@@ -1,4 +1,5 @@
 import store from '../redux/store';
+import {ProductType} from './data.types';
 
 export type RootState = ReturnType<typeof store.getState>;
 
@@ -12,4 +13,12 @@ export type AuthStateProps = {
   userUid: string;
   isSignedIn: boolean;
   isLoading: boolean;
+};
+
+export type ProductStateProps = {
+  loading: boolean;
+  products: ProductType[];
+  isFavourite: boolean;
+  error?: string;
+  nextPage: number;
 };
