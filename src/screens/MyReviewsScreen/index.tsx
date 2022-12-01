720adc9adf135f525bc1387d416457a8f739454c
hoangoreki <hoanghoutarou@gmail.com>
2022-12-01 21:43:49 +0700

    Add MyReview Screen .#75

--- src/screens/MyReviewsScreen/index.tsx
+++ src/screens/MyReviewsScreen/index.tsx
@@ -1,6 +1,5 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 import React from 'react';
-import {COLORS} from '../../constants';
 import MyReviewBox from '../../components/MyReviewBox';
 
 type Props = {};
